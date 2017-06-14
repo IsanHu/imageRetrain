776fefb18cf4430f13320e8ab86f5343d16d3492
isan <3isan333@gmail.com>
2017-06-14 14:52:08 +0800

    no message

--- importLegacy.py
+++ importLegacy.py
@@ -24,39 +24,42 @@ category = argv[1]
 categoryPath = os.path.join(imageRootPath, category)
 
 
-index = 0
-for f in sorted(os.listdir(categoryPath)):
-    if allowed_file(f):
-    	rePath = category + "/" + f
-    	print rePath
+def begin():
+	index = 0
+	for f in sorted(os.listdir(categoryPath)):
+		if allowed_file(f):
+			rePath = category + "/" + f
+			print rePath
 
-    	filePath = os.path.join(categoryPath, f)
-    	size = os.path.getsize(filePath) / 1024
-		print 'size : %d' % size
+			filePath = os.path.join(categoryPath, f)
+			size = os.path.getsize(filePath) / 1024
+			print 'size : %d' % size
 
 
-    	img = Image.open(filePath)
-		width = img.size[0]
-		height = img.size[1]
-		print width
-		print height
+			img = Image.open(filePath)
+			width = img.size[0]
+			height = img.size[1]
+			print width
+			print height
 
-		ahash8 = imagehash.average_hash(img,8)
-		ahashString8 = ahash8.__str__()
+			ahash8 = imagehash.average_hash(img,8)
+			ahashString8 = ahash8.__str__()
 
-		ahash16 = imagehash.average_hash(img,16)
-		ahashString16 = ahash16.__str__()
+			ahash16 = imagehash.average_hash(img,16)
+			ahashString16 = ahash16.__str__()
 
-		
-		index = index + 1
-		## 每10个插入一次数据
-		if index % 10 == 0:
-			print "插入数据: %d ~ %d" % (index - 10, index)
 
-		if index > 10:
-			return
+			index = index + 1
+			## 每10个插入一次数据
+			if index % 10 == 0:
+				print "插入数据: %d ~ %d" % (index - 10, index)
 
-    else:
-    	print "%s 文件类型不对" % f
+			if index > 10:
+				return
 
+		else:
+			print "%s 文件类型不对" % f
+
+
+begin()
 
